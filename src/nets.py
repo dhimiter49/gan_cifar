3702cda9e5dc45661b3e17ad44d9eac2c2f6a452
dhimiter <dhimiter.pikuli@iosb.fraunhofer.de>
2021-12-03 01:07:41 +0100

    added train, net file and dir for configuring experiments

---
+++ src/nets.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DefaultGen(nn.Module):
+    def __init__(self, parameters):
+        super().__init__()
+        pass
+
+    def forward(self, noise, label):
+        pass
+
+class DefaultDis(nn.Module):
+    def __init__(self, parameters):
+        super().__init__()
+        pass
+
+    def forward(self, image):
+        pass
---
+++ src/train.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+import nets
+
+# specify absolute path
+
+def train(model, data_loader, optimizer, loss_function):
+    pass
+
+def test(model, data_loader):
+    pass
+
+def main():
+    # read config file
+    # load dataset
+    # instantiate dataloader
+    # instantiate network, optimizer, loss...
+    # main loop, calls train and test
+    # log results
+    # plot stuff
+    pass
+
+if __name__ == "__main__":
+    main()
+
